6d097c1ac7ef3590aaf70513581e7e29cde8e5d9
Andhika Brosnan <andhikabrosnan@gmail.com>
2018-03-27 11:31:57 +0700

    Dummy data data pembanding

--- app/src/main/java/choirhelper/silihasah/org/singing/SingActivity.java
+++ app/src/main/java/choirhelper/silihasah/org/singing/SingActivity.java
@@ -1,7 +1,11 @@
 package choirhelper.silihasah.org.singing;
 
+import android.annotation.SuppressLint;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -14,24 +18,89 @@ import choirhelper.silihasah.org.R;
 
 public class SingActivity extends AppCompatActivity {
 
-    private TextView freq;
+    private TextView tv_timer;
+    private TextView tv_freq;
     private DatabaseReference mDb;
+
+    Handler handler = new Handler();
+    long startTime = 0L, timeinMilliseconds=0L, timeSwapBuff=0L, updateTime=0L;
+
+    int secs = 0;
+    int priceTrigg = 0;
+    int mins = 0;
+    int count = 0;
+
+    private SeekBar seekBar;
+    private TextView ketepatannada;
+
+    private String frequency_suara;
+
+    Runnable updateTimerThread = new Runnable() {
+        @SuppressLint({"DefaultLocale", "SetTextI18n"})
+        @Override
+        public void run() {
+            timeinMilliseconds = SystemClock.uptimeMillis() - startTime;
+            updateTime = timeSwapBuff+timeinMilliseconds;
+            secs = (int)(updateTime/1000);
+            priceTrigg = (int)(updateTime/1000);
+            mins = secs/60;
+            count++;
+
+            secs%=60;
+            int milliseconds = (int)(updateTime%1000);
+            tv_timer.setText(String.format("%2d",mins)+":"+String.format("%2d",secs)+":"+String.format("%3d",milliseconds));
+
+            //Seekbar handler
+            seekBar.setMax(50);
+            seekBar.setProgress(0);
+            seekBar.setProgress(secs);
+
+            switch (secs){
+                case 7:
+                    if(frequency_suara.equals("200")){ //Data pembanding
+                        ketepatannada.setText("NADA TEPAT");
+                    }else
+                        ketepatannada.setText("NADA SUMBANG");
+                    break;
+                case 13:
+                    if(frequency_suara.equals("130")){ //Data pembanding
+                        ketepatannada.setText("NADA TEPAT");
+                    }else
+                        ketepatannada.setText("NADA SUMBANG");
+                    break;
+                case 17:
+                    if(frequency_suara.equals("100")){ //Data pembanding
+                        ketepatannada.setText("NADA TEPAT");
+                    }else
+                        ketepatannada.setText("NADA SUMBANG");
+                default:
+                    break;
+            }
+
+            handler.postDelayed(this,0);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing);
 
-        freq = (TextView)findViewById(R.id.tv_freq);
+        tv_freq = (TextView)findViewById(R.id.tv_freq);
+        seekBar = (SeekBar)findViewById(R.id.sb_song);
+
+        tv_timer = (TextView)findViewById(R.id.tv_timer);
+        ketepatannada = (TextView)findViewById(R.id.tv_ketepatannada);
 
         mDb = FirebaseDatabase.getInstance().getReference().child("frequency");
 
         mDb.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                freq.setText(dataSnapshot.getValue().toString() +" Hz");
 
-
+                frequency_suara = dataSnapshot.getValue().toString();
+                tv_freq.setText(frequency_suara +" Hz");
             }
 
             @Override
@@ -39,5 +108,11 @@ public class SingActivity extends AppCompatActivity {
 
             }
         });
+
+        //Time Handler
+        startTime = SystemClock.uptimeMillis();
+        handler.postDelayed(updateTimerThread,0);
+
+
     }
 }
